4a18c5d9d71722b09e04ab5d837d5f012ae4e8b0
Michael Eiler <eiler.mike@gmail.com>
2021-02-18 21:03:04 +0100

    release-ubuntu.yml: initial build script using github actions

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -2,7 +2,6 @@ include(GenerateExportHeader)
 
 function (libazul_generate_export_header component_name)
     generate_export_header(azul_${component_name} EXPORT_FILE_NAME "${CMAKE_BINARY_DIR}/exports/include/azul/${component_name}/export.hpp")
-    target_include_directories(azul_${component_name} PUBLIC ${CMAKE_BINARY_DIRECTORY}/exports/include)
-    include_directories(${CMAKE_BINARY_DIR}/exports/include/)
+    target_include_directories(azul_${component_name} PUBLIC ${CMAKE_BINARY_DIR}/exports/include)
     install(FILES ${CMAKE_BINARY_DIR}/exports/include/azul/${component_name}/export.hpp DESTINATION include/azul/${component_name}/)
 endfunction()
